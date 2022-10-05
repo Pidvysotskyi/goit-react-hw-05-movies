d3c51ef0b6fbc9c66901c0f534e7efb80dea0ab9
Pidvysotskyi <Pidvysotskyi.oleksandr@gmail.com>
2022-10-05 17:57:18 +0300

    lesson

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,44 @@
+import { Routes, Route, NavLink } from 'react-router-dom';
+import { Home } from '../pages/Home/Home';
+import { About } from '../pages/About/About';
+import { Products } from '../pages/Products/Products';
+import { NotFound } from '../pages/NotFound/NotFound';
+import { ProductDetails } from '../pages/ProductDetails/ProductDetails';
+import styled from 'styled-components';
+import { Reviews } from './Reviews/Reviews';
+import { Mission } from './Mission/Mission';
+import { Team } from './Team/Team';
+
+const StyledLink = styled(NavLink)`
+  color: black;
+
+  &.active {
+    color: orange;
+  }
+`;
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <nav>
+        <StyledLink to="/" end>
+          Home
+        </StyledLink>
+        <StyledLink to="/about">About</StyledLink>
+        <StyledLink to="/products">Products</StyledLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />}>
+          <Route path="mission" element={<Mission />} />
+          <Route path="team" element={<Team />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:productId" element={<ProductDetails />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,7 @@
+export const NotFound = () => {
+  return (
+    <div>
+      <p> 404 Page is not found</p>
+    </div>
+  );
+};
