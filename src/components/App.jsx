33e314854615a6b28be29cd529de3d2465c5e36d
Pidvysotskyi <Pidvysotskyi.oleksandr@gmail.com>
2022-10-09 18:39:47 +0300

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
 import Home from 'pages/Home/Home';
 import Movies from 'pages/Movies/Movies';
 import MovieDetails from 'pages/MovieDetails/MovieDetails';
@@ -6,20 +7,24 @@ import Cast from './Cast/Cast';
 import Reviews from './Reviews/Reviews';
 import { SharedLayout } from './SharedLayout/SharedLayout';
 
+const createAsyncComponent = path => lazy(() => import(path));
+
 export const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route index element={<Home />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/movies/:movieId/" element={<MovieDetails />}>
-            <Route path="/movies/:movieId/cast" element={<Cast />} />
-            <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<SharedLayout />}>
+            <Route index element={<Home />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/movies/:movieId/" element={<MovieDetails />}>
+              <Route path="/movies/:movieId/cast" element={<Cast />} />
+              <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+            </Route>
+            <Route path="*" element={<Home />} />
           </Route>
-          <Route path="*" element={<Home />} />
-        </Route>
-      </Routes>
+        </Routes>
+      </Suspense>
     </>
   );
 };
